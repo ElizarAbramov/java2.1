debc2ce5e9df789813c716206bba95bb3ed22573
Elizar Abramov <nezha2077@mail.ru>
2021-09-29 18:33:59 +0700

    Main.java added

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPriceRUB = 10_000;
+        int MilePrice = 20;
+        int Bonus = TicketPriceRUB / MilePrice;
+        System.out.println(Bonus);
+    }
+}
